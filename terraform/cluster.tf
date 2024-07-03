3abe085dfb7e881f19a4b637f7d8fd4e7c1ab034
Jonathon Lee <jonathan.y.lee@johnlewis.co.uk>
2024-07-03 22:49:27 +1000

    refactor addons

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -5,6 +5,9 @@ module "eks" {
   cluster_name                   = local.name
   cluster_version                = local.cluster_version
   cluster_endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
+  create_cloudwatch_log_group = true
+  cloudwatch_log_group_retention_in_days = 90
 
   cluster_addons = {
     kube-proxy = { most_recent = true }
@@ -22,33 +25,60 @@ module "eks" {
     }
   }
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  node_security_group_tags = {
+    "karpenter.sh/discovery" = local.name
+  }
 
-  create_cloudwatch_log_group   = false
-  create_cluster_security_group = false
-  create_node_security_group    = false
-  authentication_mode = "API_AND_CONFIG_MAP"
-  enable_cluster_creator_admin_permissions = true
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
 
   eks_managed_node_groups = {
-    mg_5 = {
-      node_group_name = "managed-ondemand"
+    citical-addons = {
+      node_group_name = "citical-addons"
+      ami_type        = "AL2023_x86_64_STANDARD"
       instance_types  = ["t3a.medium"]
 
-      create_security_group = false
-
-      subnet_ids   = module.vpc.private_subnets
+      min_size     = 2
       max_size     = 2
       desired_size = 2
-      min_size     = 2
 
       # Launch template configuration
       create_launch_template = true              # false will use the default launch template
       launch_template_os     = "amazonlinux2eks" # amazonlinux2eks or bottlerocket
 
-      labels = {
-        intent = "control-apps"
+
+      taints = {
+        # This Taint aims to keep just EKS Addons and Karpenter running on this MNG
+        # The pods that do not tolerate this taint should run on nodes created by Karpenter
+        addons = {
+          key    = "CriticalAddonsOnly"
+          value  = "true"
+          effect = "NO_SCHEDULE"
+        },
+      }
+    }
+  }
+
+  access_entries = {
+    "allow-root-user" = {
+      kubernetes_groups = []
+      principal_arn     = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+
+      policy_associations = {
+        namespaced = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            namespaces = ["default"]
+            type       = "namespace"
+          }
+        }
+        cluster = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
       }
     }
   }
